d922827b1083445b6072dcb7f980e19853e164c3
RoAd-ZU <ZUZERN.AD@gmail.com>
2023-10-25 20:44:59 +0300

    rollback migrations

--- catalog/models.py
+++ catalog/models.py
@@ -8,12 +8,13 @@ class Product(models.Model):
     image = models.ImageField(upload_to='product/', verbose_name='превью', **NULLABLE)
     category = models.CharField(max_length=100, verbose_name='категория')
     price = models.IntegerField(verbose_name='цена')
-    date_cr = models.DateField(verbose_name='дата создания')
-    date_ch = models.DateField(verbose_name='дата последнего изменения')
+    date_cr = models.DateField(verbose_name='дата создания', **NULLABLE)
+    date_ch = models.DateField(verbose_name='дата последнего изменения', **NULLABLE)
 
 class Category(models.Model):
     name = models.CharField(max_length=100, verbose_name='наименование')
     description = models.CharField(max_length=100, verbose_name='описание')
+    created_at = models.CharField(max_length=50, verbose_name='будет отменено', **NULLABLE)
 
 class Meta:
     ordering = ('name',)
